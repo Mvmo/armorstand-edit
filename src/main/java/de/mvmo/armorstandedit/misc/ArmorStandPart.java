9757de8daf41a1d6ab6a05028986e7adcc604e43
Maurice Schmidt <mvmogo@gmail.com>
2019-07-08 06:49:50 +0200

    create enum representing each part of an armorstand

---
+++ src/main/java/de/mvmo/armorstandedit/misc/ArmorStandPart.java
@@ -0,0 +1,17 @@
+package de.mvmo.armorstandedit.misc;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum ArmorStandPart {
+
+    HEAD(0),
+    BODY(1),
+    RIGHT_ARM(2),
+    LEFT_ARM(3),
+    RIGHT_LEG(4),
+    LEFT_LEG(5);
+
+    private int index;
+
+}
