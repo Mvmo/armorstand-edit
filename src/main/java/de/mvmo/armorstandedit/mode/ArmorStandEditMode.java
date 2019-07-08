3a8f8a48fdfc1028c67ac5d4e5baf5c5ebf899a3
Maurice Schmidt <mvmogo@gmail.com>
2019-07-08 07:41:00 +0200

    some hacky shit... need to redo after sleep

--- src/main/java/de/mvmo/armorstandedit/mode/ArmorStandEditMode.java
+++ src/main/java/de/mvmo/armorstandedit/mode/ArmorStandEditMode.java
@@ -1,6 +1,7 @@
 package de.mvmo.armorstandedit.mode;
 
 import com.google.common.collect.Lists;
+import de.mvmo.armorstandedit.misc.ArmorStandPart;
 import de.mvmo.armorstandedit.misc.Axis;
 import de.mvmo.armorstandedit.util.ItemStackBuilder;
 import de.mvmo.armorstandedit.util.TexturedSkullItem;
@@ -11,7 +12,9 @@ import lombok.Setter;
 import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
+import org.bukkit.util.EulerAngle;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 @Getter
@@ -28,11 +31,12 @@ public class ArmorStandEditMode {
     private final Player player;
 
     private ArmorStand armorStand;
-    private Axis axis;
+    private ArmorStandPart part;
 
     public static ArmorStandEditMode of(Player player) {
         ArmorStandEditMode editMode = new ArmorStandEditMode(player);
 
+        editMode.part = ArmorStandPart.HEAD;
         EDIT_MODE_LIST.add(editMode);
 
         return editMode;
@@ -51,6 +55,55 @@ public class ArmorStandEditMode {
                     .build());
             return;
         }
+
+        player.getInventory().setItem(0, new ItemStackBuilder(TexturedSkullItem.create("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNWE2Nzg3YmEzMjU2NGU3YzJmM2EwY2U2NDQ5OGVjYmIyM2I4OTg0NWU1YTY2YjVjZWM3NzM2ZjcyOWVkMzcifX19"))
+                .setDisplayName("§cX")
+                .build());
+
+        player.getInventory().setItem(1, new ItemStackBuilder(TexturedSkullItem.create("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzUyZmIzODhlMzMyMTJhMjQ3OGI1ZTE1YTk2ZjI3YWNhNmM2MmFjNzE5ZTFlNWY4N2ExY2YwZGU3YjE1ZTkxOCJ9fX0="))
+                .setDisplayName("§cY")
+                .build());
+
+        player.getInventory().setItem(2, new ItemStackBuilder(TexturedSkullItem.create("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOTA1ODJiOWI1ZDk3OTc0YjExNDYxZDYzZWNlZDg1ZjQzOGEzZWVmNWRjMzI3OWY5YzQ3ZTFlMzhlYTU0YWU4ZCJ9fX0="))
+                .setDisplayName("§cZ")
+                .build());
+    }
+
+    public EulerAngle executeRotation(Axis axis) {
+        int x = axis.equals(Axis.X) ? 5 : 0;
+        int y = axis.equals(Axis.Y) ? 5 : 0;
+        int z = axis.equals(Axis.Z) ? 5 : 0;
+
+        try {
+            Method poseGetMethod = armorStand.getClass().getDeclaredMethod("get" + part.getFormattedName() + "Pose");
+            poseGetMethod.setAccessible(true);
+
+            EulerAngle angle = (EulerAngle) poseGetMethod.invoke(armorStand);
+
+            Method axisGetMethod = angle.getClass().getDeclaredMethod("get" + axis.toString());
+            axisGetMethod.setAccessible(true);
+
+            double axisValue = (double) axisGetMethod.invoke(angle);
+            if (Math.toDegrees(axisValue) > 360)
+                axisValue = 0;
+            if (Math.toDegrees(axisValue) < 0)
+                axisValue = 360;
+
+            Method axisSetMethod = angle.getClass().getDeclaredMethod("set" + axis.toString(), Double.class);
+            axisSetMethod.setAccessible(true);
+
+            axisSetMethod.invoke(angle, axisValue);
+
+            Method poseSetMethod = armorStand.getClass().getDeclaredMethod("set" + part.getFormattedName() + "Pose", EulerAngle.class);
+            poseSetMethod.setAccessible(true);
+            poseSetMethod.invoke(armorStand, angle);
+
+            return angle;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return new EulerAngle(0, 0, 0);
     }
 
     public static boolean isInEditMode(Player player) {
