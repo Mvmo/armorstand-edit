a4805a50a348469b7c995859061b67051aa13ea6
Maurice Schmidt <mvmogo@gmail.com>
2019-07-08 05:15:15 +0200

    register the command

--- src/main/java/de/mvmo/armorstandedit/ArmorStandEditPlugin.java
+++ src/main/java/de/mvmo/armorstandedit/ArmorStandEditPlugin.java
@@ -1,4 +1,17 @@
 package de.mvmo.armorstandedit;
 
-public class ArmorStandEditPlugin {
+import de.mvmo.armorstandedit.command.ArmorStandEditCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * This is the class where it all comes together and everything has a beautiful time with each other.
+ * Also the Bukkit-Server will call this class as soon as Bukkit thinks the time has come to call this class for example on the startup of the server.
+ */
+public class ArmorStandEditPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getCommand("armorstandedit").setExecutor(new ArmorStandEditCommand());
+    }
+
 }
