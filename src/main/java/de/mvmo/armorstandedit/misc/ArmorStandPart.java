d50c713f4bf002a066e46c287ee2dd088bb727a4
Maurice Schmidt <mvmogo@gmail.com>
2019-07-08 07:41:12 +0200

    add formattedName field

--- src/main/java/de/mvmo/armorstandedit/misc/ArmorStandPart.java
+++ src/main/java/de/mvmo/armorstandedit/misc/ArmorStandPart.java
@@ -1,18 +1,21 @@
 package de.mvmo.armorstandedit.misc;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public enum ArmorStandPart {
 
-    HEAD(0),
-    BODY(1),
-    RIGHT_ARM(2),
-    LEFT_ARM(3),
-    RIGHT_LEG(4),
-    LEFT_LEG(5);
+    HEAD(0, "Head"),
+    BODY(1, "Body"),
+    RIGHT_ARM(2, "RightArm"),
+    LEFT_ARM(3, "LeftArm"),
+    RIGHT_LEG(4, "RightLeg"),
+    LEFT_LEG(5, "LeftLeg");
 
     private int index;
+    private String formattedName;
 
     private ArmorStandPart next() {
         int index = this.index + 1;
