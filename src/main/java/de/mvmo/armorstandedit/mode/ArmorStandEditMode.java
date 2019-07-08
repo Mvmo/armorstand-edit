1c8b4e893e9dbc80a5ad622fa6078d4014cfe9de
Maurice Schmidt <mvmogo@gmail.com>
2019-07-08 05:14:53 +0200

    create ArmorStandEditMode class it will manage the edit mode

---
+++ src/main/java/de/mvmo/armorstandedit/mode/ArmorStandEditMode.java
@@ -0,0 +1,53 @@
+package de.mvmo.armorstandedit.mode;
+
+import com.google.common.collect.Lists;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ArmorStandEditMode {
+
+    private static final List<ArmorStandEditMode> EDIT_MODE_LIST;
+
+    static {
+        EDIT_MODE_LIST = Lists.newArrayList();
+    }
+
+    private final Player player;
+
+    private ArmorStand armorStand;
+
+    public static ArmorStandEditMode of(Player player) {
+        ArmorStandEditMode editMode = new ArmorStandEditMode(player);
+
+        EDIT_MODE_LIST.add(editMode);
+
+        return editMode;
+    }
+
+    public void applyInventory() {
+        if (armorStand == null) {
+            player.getInventory().setItem(0, new ItemStack(Material.STONE));
+            return;
+        }
+    }
+
+    public static boolean isInEditMode(Player player) {
+        return EDIT_MODE_LIST.stream()
+                .anyMatch(editMode -> editMode.player.equals(player));
+    }
+
+    public static ArmorStandEditMode editModeFromPlayer(Player player) {
+        return EDIT_MODE_LIST.stream()
+                .filter(editMode -> editMode.player.equals(player))
+                .findFirst()
+                .orElse(null);
+    }
+
+}
